ac34648f9bf8a3feeb6573255bba4e528f11bcaf
rafael3007 <rafael.brito.14222@gmail.com>
2022-01-03 23:42:41 -0300

    parte 1

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>HomePage</title>
+      </Head>
+
+      <main>
+        <h1>Hellow World</h1>
+      </main>
+    </div>
+  )
+}
+
+
+export default Home
